35a322f90c4ac0c9c013511cac3c3c623df9be21
abhimanyu-beep <76608899+abhimanyu-beep@users.noreply.github.com>
2022-03-28 21:11:25 +0530

    Add files via upload

---
+++ c129/final.py
@@ -0,0 +1,25 @@
+import csv
+from email import header
+import enum
+dataset1=[]
+dataset2=[]
+with open("dataset_1.csv","r")as f:
+    csvreader=csv.reader(f)
+    for row in csvreader:
+        dataset1.append(row)
+with open("newdataset.csv","r")as f:
+    csvreader=csv.reader(f)
+    for row in csvreader:
+        dataset2.append(row)
+header1=dataset1[0]
+planetdata1=dataset1[1:]
+header2=dataset2[0]
+planetdata2=dataset2[1:]
+header=header1+header2
+planetdata=[]
+for index,datarow in enumerate(planetdata1):
+    planetdata.append(planetdata1[index]+planetdata2[index])
+with open("final.csv","a+")as f:
+    csvwriter=csv.writer(f)
+    csvwriter.writerow(header)
+    csvwriter.writerows(planetdata)
---
+++ c129/main.py
@@ -0,0 +1,16 @@
+import csv
+from email import header
+data=[]
+with open("dataset_2.csv","r")as f:
+    csvreader=csv.reader(f)
+    for row in csvreader:
+        data.append(row)
+headers=data[0]
+planetdata=data[1:]
+for datapoint in planetdata:
+    datapoint[2]=datapoint[2].lower()
+planetdata.sort(key=lambda planetdata:planetdata[2])
+with open("newdataset.csv","a+")as f:
+    csvwriter=csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(planetdata)
